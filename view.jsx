b951da862deb27cb37dcf1efd739a8e9373964c7
Heena Chaudhary <87467092+heenasc@users.noreply.github.com>
2023-08-23 09:00:34 +0530

    Add files via upload

--- view.jsx
+++ view.jsx
@@ -1,27 +1,45 @@
 import ForgeUI, { User,  DashboardGadget, useState, useEffect, Text , useProductContext,Select, Option }  from "@forge/ui";
 import api, { route } from "@forge/api";
-
 const View = () => {
  
+
   const [logwork, setLogwork] = useState("Fetching logwork...");
   let [start,setStartDate] = useState('');
   let [End,setEndDate] = useState('');
   let [loggedWork,setLoggedWork] = useState(false)
   let UserWorkLogs = {};
   let [worklogs,setworklogs] = useState(false);
-  let [project, setProject] = useState("");
+  let [projects, setProject] = useState("");
+  let [accountIds, setaccountIds] = useState("");
+  const [selectedRoleUserAccountIds, setSelectedRoleUserAccountIds] = useState([]);
   const { extensionContext: { gadgetConfiguration } } = useProductContext();
-  //const { User, StartDate, EndDate } = gadgetConfiguration;
-  const { User, StartDate, EndDate, Project } = gadgetConfiguration;
-  console.log(User,StartDate,EndDate,Project,"This is from useEffect THigns")
-  // useState(fetchNumberOfIssues(User,StartDate,EndDate));
-  useState(somethingElse(User,StartDate,EndDate));
-  async function timer(time){
+  const someRandomvalue = useProductContext();
+  
+  const { roles,StartDate, EndDate,project } = gadgetConfiguration;
+console.log("This is roles",roles);
+console.log("This is project",project);
+
+  console.log("This is after someRandomvalue",someRandomvalue);
+ async function timer(time){
     return new Promise((resolve,reject)=>{
       setTimeout(()=>{resolve()},time)
     })
   }
-  async function fetchNumberOfIssues(User, startDate , EndDate,Project) {
+  // console.log(value);
+  // useState(fetchNumberOfIssues(User,StartDate,EndDate));
+  useState(fetchRoles());
+  useState(somethingElse(accountIds,StartDate,EndDate));
+  
+  
+  
+  async function fetchRoles() {
+      const roleUsersResponse = await api.asUser().requestJira(route`/rest/api/3/role/${roles}`);
+      const roleUsersJson = await roleUsersResponse.json();
+      const accountIds = roleUsersJson.actors.map((actor) => actor.actorUser.accountId);
+      setSelectedRoleUserAccountIds(accountIds);
+      console.log("accountIds",accountIds);
+    }
+  async function fetchNumberOfIssues(accountIds, startDate , EndDate,project) {
     
     // console.log(User , "This is from fetchNumberOfIssues");
     // console.log(startDate,EndDate);
@@ -29,26 +47,26 @@ const View = () => {
     setEndDate(EndDate);
     // console.log(start,End),"This sithe values";
     let UserWorkLogs = {};
-    for(let i in User){
-      UserWorkLogs[User[i]] = 0
+    for(let i in accountIds){
+      UserWorkLogs[accountIds[i]] = 0
     }
 
     // console.log(UserWorkLogs);
 
 
-    for(let i in User){
-      let workLog = await fetchIssues(User[i],startDate,EndDate,Project);
+    for(let i in accountIds){
+      let workLog = await fetchIssues(accountIds[i],startDate,EndDate,project);
     }
   }
  
   async function somethingElse(){
  
-    for(let i in User){
-      UserWorkLogs[User[i]] = 1
+    for(let i in accountIds){
+      UserWorkLogs[accountIds[i]] = 1
     }
-    for(let i in User){
-      let workLog = await fetchIssues(User[i],StartDate,EndDate, Project);
-      UserWorkLogs[User[i]] = workLog;
+    for(let i in accountIds){
+      let workLog = await fetchIssues(accountIds[i],StartDate,EndDate, project);
+      UserWorkLogs[accountIds[i]] = workLog;
     }
     
     console.log(UserWorkLogs),"This is worklogs userWorklogs";
@@ -57,16 +75,16 @@ const View = () => {
     setworklogs(UserWorkLogs);
 
 }
-  async function fetchIssues(user,start,end,Project){
+  async function fetchIssues(accountIds,start,end,project){
     
-    console.log(user);
+    console.log(accountIds);
     console.log(start,end);
     let issues = []
     let loop = true;
     let startAt = 0;
     let maxResults = 50;
 
-    const jqlQuery = `project = "${Project}" AND worklogAuthor in (${user}) AND updated >= "${start}" AND updated <= "${end}"`;
+    const jqlQuery = `project = "${project}" AND worklogAuthor in (${accountIds}) AND updated >= "${start}" AND updated <= "${end}"`;
     console.log(jqlQuery);
     while(loop){
       const response = await api.asUser().requestJira(route`/rest/api/3/search?jql=${jqlQuery}&fields=none&maxResults=${maxResults}&startAt=${startAt}`,{
@@ -87,7 +105,7 @@ const View = () => {
     // for(let i in issues){
     //   let issueWorklogs = await workLogs(issues,start,end);
     // }
-    let userWorkLogs = await workLogs(issues,start,end,user)
+    let userWorkLogs = await workLogs(issues,start,end,accountIds)
 
     return userWorkLogs;
     
@@ -95,7 +113,7 @@ const View = () => {
 
   }
 
-  async function workLogs(userIssueIds,start,end,user){
+  async function workLogs(userIssueIds,start,end,accountIds){
     // console.log("this is from worklogsfunction",userIssueIds,start,end);
     let worklogs = [];
     for(let i in userIssueIds){
@@ -186,7 +204,6 @@ const View = () => {
   
   return (
     <DashboardGadget>
-    
    {loggedWork && details()}
 
     </DashboardGadget>
@@ -194,7 +211,3 @@ const View = () => {
 };
 
 export default View;
-
-
-
-
